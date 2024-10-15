e4e20ae73bd6bc10fa42f6a1ea6ed75961be0ff5
Mirla Braga <mirla.chucre@dft.gov.uk>
2024-10-15 10:50:44 +0100

    import Auth0 provide to the application module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,8 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { provideAuth0 } from '@auth0/auth0-angular';
+import * as ProvideAuth0 from "@auth0/auth0-angular";
+
 import { HomePageModule } from './home-page/home-page.module';
 import { LoginButtonModule } from './login-button/login-button.module';
 
@@ -15,15 +16,16 @@ import { LoginButtonModule } from './login-button/login-button.module';
     BrowserModule,
     AppRoutingModule,
     HomePageModule,
-    LoginButtonModule
+    LoginButtonModule,
+    ProvideAuth0.AuthModule.forRoot({
+      domain: 'Yhttp://localhost:3000',
+      clientId: 'gS3C20Vt9iT7qy2yprLHjw7SuK8IDWjQ',
+      authorizationParams: {
+        redirect_uri: window.location.origin,
+      },
+    })
   ],
-  providers: [provideAuth0({
-    domain: 'http://localhost:3000',
-    clientId: 'gS3C20Vt9iT7qy2yprLHjw7SuK8IDWjQ',
-    authorizationParams: {
-      redirect_uri: window.location.origin
-    }
-  })],
+  providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
